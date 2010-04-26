bbb7903e723aab3c63c2b02ba56164f25dbe1b52
Nate Eagle <nate@nateeagle.com>
2010-04-26 15:54:04 -0700

    So much awesome: universalized all motions into move and motions objects, changed open and close to use them

--- src/jquery.pinwheel.path.js
+++ src/jquery.pinwheel.path.js
@@ -70,253 +70,114 @@ var Pinwheel = {
 
             // Save path for each petal
             item.data('arc', new $.path.arc(arc_params));
-
-            // console.log(item.arc);
         });
 
         if (this.options.open == true) {
             this.open();
         }
     },
-    open: function(options) {
-        options = options || {
-            duration: 1000,
-            easing: 'easeOutQuad'
-        }
 
-        var that = this;
-
-        that.state.open = true;
-
-        this.petals.each(function(i, item) {
-            var $item = $(item);
-
-            // Retrieve arc from item
-            var arc = $item.data('arc');
-            arc.start = i * that.petalSpace + that.options.home;
-            arc.end = arc.start;
-            arc.x = arc.css(0).left;
-            arc.y = arc.css(0).top;
-
-            $item.data('angle', arc.start);
-
-            var bezier_params = {
-                start: {
-                    x: $item.offset().left,
-                    y: $item.offset().top,
-                    angle: 90,
-                    length: .5
-                },
-                end: {
-                    // Use the arc coordinates as end points
-                    x: parseInt(arc.x),
-                    y: parseInt(arc.y),
-                    angle: -90,
-                    length: .5 
-                }
-            }
-
-            $item.stop().animate({
-                opacity: that._alpha[1],
-                path: new $.path.bezier(bezier_params)
-            }, {
-                duration: options.duration,
-                easing: options.easing 
-            });
-
-        });
-    },
-
-    close: function(dir) {
-        var dir = dir || 1;
-        var that = this;
-
-        that.state.open = false; 
-
-        this.petals.each(function(i, item) {
-            var $item = $(item);
-            var dimensions = [that.options.origin[0] - ($item.outerWidth() / 2), that.options.origin[1] - ($item.outerHeight() / 2)];
-            
-            var bezier_params = {
-                start: {
-                    x: $item.offset().left,
-                    y: $item.offset().top,
-                    angle: 90 * dir,
-                    length: .5
-                },
-                end: {
-                    x: dimensions[0],
-                    y: dimensions[1],
-                    angle: 90 * -dir,
-                    length: .5 
-                }
-            }
-
-            $item.stop().animate({
-                opacity: that._alpha[0],
-                path: new $.path.bezier(bezier_params)
-            }, {
+    /* MOVE:
+     * The workhorse of the pinwheel
+     * Motions are stored in a separate motions object that belongs to Pinwheel, and can be extended
+     */ 
+    move: function(degrees, options) {
+        var that = this,
+            movement,
+            settings = {
+                complete: function() {},
+                _delay: 0,
                 duration: 1000,
-                easing: 'easeInQuad'
-            });
+                easing: 'jswing',
+                how: 'spin',
+                petals: this.petals, // Default is to move all petals
+                radius: 1,
+                stagger: false,
+                sequential: false
+            };
+        $.extend(settings, options);
 
-        });
 
-    },
-
-    // Spin either counterclockwise (default) or clockwise
-    spin: function(degrees, direction, callback) {
-        var callback = callback || function() {}
-        var that = this;
-        var arc_params = {
-            center: this.options.origin,
-            radius: this.options.radius,
-            dir: direction || 1
-        }
-
-        this.petals.each(function(i, item) {
-            var $item = $(item);
-            var dimensions = [that.options.origin[0] - ($item.outerWidth() / 2), that.options.origin[1] - ($item.outerHeight() / 2)];
-            
-            arc_params.center = dimensions;
-            arc_params.start = $item.data('angle'); 
-            arc_params.end = arc_params.start + degrees; 
-
-            $(item).delay((Math.random() * 10) * 50).animate({
-                path: new $.path.arc(arc_params)
-            }, {
-                complete: function() {
-                    // Only fire callback once
-                    if(i == 0) {
-                        callback();
-                    }
-                },
-                duration: 1000,
-                easing: 'easeOutQuint',
-                step: function(options, attributes) {
-                    $(this).data('angle', $(this).css('angle'));
-                }
-            });
-        })
-    },
-
-    // Spin to a given petal
-    // Expects a jQuery-wrapped object
-    spinTo: function(petal, callback) {
-        var ang, degrees, dir;
-        var callback = callback || {};
-
-        ang = petal.data('angle') || 0;
-        ang = (ang + 360) % 360;
-        degrees = (this.options.home - ang);
-        if (degrees > 0) { dir = 1; } else { dir = -1; }
-        this.spin(degrees, dir, callback);
-    },
-    dart: function(degrees, callback) {
-        var callback = callback || function() {}
-        var that = this;
-
+        // Retrieve the starting angle of the pinwheel
         var startAng = that.state.angle;
         that.state.angle = (that.state.angle + degrees) % 360; 
 
-        this.petals.each(function(i, item) {
-            var $item = $(item);
 
-            // Retrieve arc from item
+        settings.petals.each(function(i, item) {
+            $item = $(item);
+
+            /* STAGGER */
+            // Preface each animation with a random delay
+            if(settings.stagger == true) {
+                settings._delay = (Math.random() * 10) * 50;
+            }
+
+            /* SEQUENTIAL */
+            // Animate petals successively
+            if(settings.sequential == true) {
+                settings._delay = i * (settings.duration / settings.petals.length);
+            }
+
+            // Get ending coordinates for each object 
             var arc = $item.data('arc');
+            arc.radius = that.options.radius * settings.radius;
             arc.start = i * that.petalSpace + startAng;
             arc.end = degrees + arc.start;
-            // console.log('Start:', arc.start, 'Degrees:', degrees, 'End:', arc.end);
             arc.x = arc.css(0).left;
             arc.y = arc.css(0).top;
 
-            $(item).delay((Math.random() * 10) * 50).animate({
+            // Animate using the specified motion
+            var animation = that.motions[settings.how]($item, arc);
+            // Add any additional animations in the settings
+            $.extend(animation, settings.animation);
+
+            $item.delay(settings._delay).animate(animation, {
+                complete: function() {
+                    // Only fire callback once
+                    // replace with .once()?
+                    if(i == 0) {
+                        settings.complete();
+                    }
+                },
+                duration: settings.duration,
+                easing: settings.easing
+            });
+        });
+    },
+    // The home of pinwheel motions
+    // Extensible! Add motions dynamically.
+    motions: {
+        dart: function($item, arc) {
+            return {
                 left: arc.x,
                 top: arc.y
-            }, {
-                complete: function() {
-                    // Only fire callback once
-                    if(i == 0) {
-                        callback();
-                    }
-                },
-                duration: 1000,
-                easing: 'easeOutQuint',
-                step: function(options, attributes) {
-                }
-            });
-        });
-
-    },
-    iris: function(degrees, callback) {
-        var callback = callback || function() {}
-        var that = this;
-
-        var startAng = that.state.angle;
-        that.state.angle = (that.state.angle + degrees) % 360; 
-
-        this.petals.each(function(i, item) {
-            var $item = $(item);
-
-            // Retrieve arc from item
-            var arc = $item.data('arc');
-            arc.start = i * that.petalSpace + startAng;
-            arc.end = degrees + arc.start;
-            console.log('Start:', arc.start, 'Degrees:', degrees, 'End:', arc.end);
-            arc.x = arc.css(0).left;
-            arc.y = arc.css(0).top;
-
+            }
+        },
+        iris: function($item, arc) {
             var bezier_params = {
                 start: {
                     x: $item.offset().left,
                     y: $item.offset().top,
-                    angle: 90,
+                    angle: -90,
                     length: .5
                 },
                 end: {
                     // Use the arc coordinates as end points
                     x: parseInt(arc.x),
                     y: parseInt(arc.y),
-                    angle: -90,
+                    angle: 90,
                     length: .5 
                 }
             }
 
-            $(item).animate({
-            // $(item).delay((Math.random() * 10) * 50).animate({
-                path: new $.path.bezier(bezier_params)
-            }, {
-                complete: function() {
-                    // Only fire callback once
-                    if(i == 0) {
-                        callback();
-                    }
-                },
-                duration: 1000,
-                easing: 'easeOutQuint',
-                step: function(options, attributes) {
-                }
-            });
-        });
+            return { path: new $.path.bezier(bezier_params) };
+        },
 
-    },
-    swarm: function(degrees, callback) {
-        var callback = callback || function() {}
-        var that = this;
+        spin: function($item, arc) {
+            return { path: new $.path.arc(arc) }
+        },
 
-        var startAng = that.state.angle;
-        that.state.angle = (that.state.angle + degrees) % 360; 
-
-        this.petals.each(function(i, item) {
-            var $item = $(item);
-
-            // Retrieve arc from item
-            var arc = $item.data('arc');
-            arc.start = i * that.petalSpace + startAng;
-            arc.end = degrees + arc.start;
-            arc.x = arc.css(0).left;
-            arc.y = arc.css(0).top;
-
+        swarm: function($item, arc) {
             var bezier_params = {
                 start: {
                     x: $item.offset().left,
@@ -332,24 +193,55 @@ var Pinwheel = {
                     length: Math.random()
                 }
             }
+            
+            return { path: new $.path.bezier(bezier_params) };
+        }
+    },
 
-            // $(item).animate({
-            $(item).delay((Math.random() * 10) * 50).animate({
-                path: new $.path.bezier(bezier_params)
-            }, {
-                complete: function() {
-                    // Only fire callback once
-                    if(i == 0) {
-                        callback();
-                    }
-                },
-                duration: 1000,
-                easing: 'easeInOutBack',
-                step: function(options, attributes) {
-                }
-            });
-        });
+    open: function(options) {
+        var settings = {
+            animation: {
+                opacity: this._alpha[1] 
+            },
+            how: 'dart',
+            duration: 500,
+            radius: 1,
+            complete: function() {}
+        }
 
+        $.extend(settings, options);
+
+        this.state.open = true;
+
+        this.move(0, settings);
+    },
+
+    close: function(options) {
+        var settings = {
+            animation: {
+                opacity: this._alpha[0]
+            },
+            how: 'iris',
+            sequential: true,
+            radius: 0
+        }
+        $.extend(settings, options);
+
+        this.state.open = false;
+        this.move(0, settings);
+    },
+
+    // Spin to a given petal
+    // Expects a jQuery-wrapped object
+    spinTo: function(petal, callback) {
+        var ang, degrees, dir;
+        var callback = callback || {};
+
+        ang = petal.data('angle') || 0;
+        ang = (ang + 360) % 360;
+        degrees = (this.options.home - ang);
+        if (degrees > 0) { dir = 1; } else { dir = -1; }
+        this.spin(degrees, dir, callback);
     }
 }
 
